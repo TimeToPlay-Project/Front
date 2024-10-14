02c8192d4e4a41e28e3973bb701f13546a180e55
sk <78210333+sk-zo@users.noreply.github.com>
2024-10-14 14:41:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CommonRoutes from "./Routes/CommonRoutes";
 import QuizRoutes from "./Routes/QuizRoutes";
 import TestRoute from "./Routes/TestRoute";
-
+import TournamentRoute from "./Routes/TournamentRoute";
 
 function App() {
   return (
@@ -17,6 +15,9 @@ function App() {
         <Route path="/quiz/*" element={<QuizRoutes />} />
 
         <Route path="/test/*" element={<TestRoute />} />
+
+        <Route path="/tournament/*" element={<TournamentRoute />} />
+
 
       </Routes>
     </Router>
