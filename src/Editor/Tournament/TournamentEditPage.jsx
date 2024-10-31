9d4db43e6afde497e5e4bc2a89ddfb61891837a9
sk-jo <gajeong01@gmail.com>
2024-10-31 16:39:59 +0900

    Add fetch tournament edit data

--- src/Editor/Tournament/TournamentEditPage.jsx
+++ src/Editor/Tournament/TournamentEditPage.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 
-function TournamentEditPage(id) {
-    const [tournamentData, setTournamentData] = useState([]);
-
+function TournamentEditPage({id}) {
+    const [tournamentData, setTournamentData] = useState(null);
     useEffect(() => {
         if (id !== "new") {
-            
+            fetch(`${process.env.REACT_APP_API_URL}/api/editor/tournament/${id}`)
+            .then(response => response.json())
+            .then(data => setTournamentData(data))
+            .catch(error => console.error('Error fetching tournamentEditData:', error));
         }
-    }, [id])
+    }, [id]);
+
+    console.log(tournamentData);
     
     return (
         <div>
