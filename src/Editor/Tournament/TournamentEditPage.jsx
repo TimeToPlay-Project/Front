6c67141092c1e4e672fb20fd6d6c452129e5767d
sk-jo <gajeong01@gmail.com>
2024-11-01 15:23:49 +0900

    Add tournamentEditPage

--- src/Editor/Tournament/TournamentEditPage.jsx
+++ src/Editor/Tournament/TournamentEditPage.jsx
@@ -1,29 +1,212 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./css/TournamentEditPage.css";
 
+function TournamentEditPage({ id }) {
+    const [tournamentData, setTournamentData] = useState({
+        tournament: { title: '', description: '', thumbnail: '', is_update: false },
+        images: [],
+    });
+    const [thumbnailFile, setThumbnailFile] = useState(null);
+    const [imageFiles, setImageFiles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-function TournamentEditPage({id}) {
-    const [tournamentData, setTournamentData] = useState(null);
     useEffect(() => {
         if (id !== "new") {
             fetch(`${process.env.REACT_APP_API_URL}/api/editor/tournament/${id}`)
-            .then(response => response.json())
-            .then(data => setTournamentData(data))
-            .catch(error => console.error('Error fetching tournamentEditData:', error));
+                .then(response => response.json())
+                .then(data => {
+                    setTournamentData(data);
+                    setImageFiles(new Array(data.images.length).fill(null));
+                    setIsLoading(false);
+                })
+                .catch(error => {
+                    console.error('Error fetching tournamentEditData:', error);
+                    setIsLoading(false);
+                });
+        } else {
+            setIsLoading(false);
         }
     }, [id]);
 
-    console.log(tournamentData);
+    console.log("tournamentData: ", tournamentData);
+    console.log("imageFiles: ", imageFiles);
+
+    const handleImageUpload = (file, type, index = null) => {
+        console.log(index);
+        const newUrl = URL.createObjectURL(file);
+        console.log(newUrl);
+        if (type === "tournament") {
+            handleFieldChange(type, "thumbnail", newUrl);
+            setThumbnailFile(file);
+        } else if (type === "image" && index !== null) {
+            handleFieldChange(type, "image_url", newUrl, index);
+            setImageFiles((prev) => {
+                const updatedImageFiles = [...prev];
+                updatedImageFiles[index] = file;
+                return updatedImageFiles;
+            });
+        }
+    };
+
+    const handleFieldChange = (type, field, value, index = null) => {
+        if (type === "tournament") {
+            setTournamentData(prev => ({
+                ...prev,
+                tournament: {
+                    ...prev.tournament,
+                    [field]: value,
+                    is_update: true,
+                }
+            }))
+        } else if (type === "image" && index !== null) {
+            setTournamentData(prev => {
+                const updatedImages = [...prev.images];
+                updatedImages[index] = {
+                    ...updatedImages[index],
+                    [field]: value,
+                    is_update: true,
+                };
+                return { ...prev, images: updatedImages};
+            });
+        }
+    }
+
+    const handleEditSubmit = async() => {
+        const formData = new FormData();
+        formData.append('tournamentData', JSON.stringify(tournamentData));
+        formData.append('thumbnail', thumbnailFile);
+
+        imageFiles.forEach((file) => {
+            if (file) {
+                formData.append(`images`, file);
+            } else {
+                const emptyFile = new Blob([''], { type: 'image/png' });
+                formData.append('images', emptyFile, 'empty.png');
+            }
+        });
+        
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/editor/tournament/submit/${id}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
     
     return (
-        <div>
-            <div>sss</div>
-            <div>
-                <button>Main</button>
+        <div className="tournament-edit-container">
+            <div className="tournament-edit-box">
+                <span>썸네일</span>
+                <div className="tournament-thumbnail-box">
+                    <label
+                        htmlFor="tournament-thumbnail-input"
+                        className="tournament-thumbnail-label"
+                        style={{
+                            backgroundImage: tournamentData?.tournament?.thumbnail 
+                                ? `url(${tournamentData.tournament.thumbnail.startsWith('blob') 
+                                    ? tournamentData.tournament.thumbnail 
+                                    : `${process.env.REACT_APP_API_URL}/${tournamentData.tournament.thumbnail}`})` 
+                                : `url('/image.png')`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                        }}
+                    >
+                    </label>
+                    <input
+                        id="tournament-thumbnail-input"
+                        className="tournament-file-input"
+                        accept=".png, .jpeg, .jpg"
+                        type="file"
+                        onChange={(e) => {
+                            if (e.target.files[0]) {
+                                handleImageUpload(e.target.files[0], "tournament");
+                            }
+                        }}
+                    />
+                </div>
+            
+                <div className="tournament-text-input-box">
+                    <span>제목</span>
+                    <div>
+                        <input
+                            className="tournament-text-input"
+                            type="text"
+                            value={tournamentData?.tournament?.title || ''} 
+                            onChange={(e) => handleFieldChange('tournament', 'title', e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="tournament-text-input-box">
+                    <span>설명</span>
+                    <div>
+                        <input
+                            className="tournament-text-input"
+                            type="text"
+                            value={tournamentData?.tournament?.description || ''} 
+                            onChange={(e) => handleFieldChange('tournament', 'description', e.target.value)}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="tournament-image-edit-container">
+                {tournamentData.images.map((image, index) => (
+                    <div key={image.id} className="tournament-image-edit-box">
+                        <div className="tournament-thumbnail-box">
+                            <label
+                                htmlFor={`tournament-image-input-${index}`}
+                                className="tournament-image-label"
+                                style={{
+                                    backgroundImage: image.image_url 
+                                        ? `url(${image.image_url.startsWith('blob') 
+                                            ? image.image_url
+                                            : `${process.env.REACT_APP_API_URL}/${image.image_url}`})` 
+                                        : `url('/image.png')`,
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "center",
+                                }}
+                            >
+                            </label>
+                            <input
+                                id={`tournament-image-input-${index}`}
+                                className="tournament-file-input"
+                                accept=".png, .jpeg, .jpg"
+                                type="file"
+                                onChange={(e) => {
+                                    if (e.target.files[0]) {
+                                        handleImageUpload(e.target.files[0], "image", index);
+                                    }
+                                }}
+                            />
+                        </div>
+                        <div className="tournament-text-input-box">
+                            <span>이름</span>
+                            <div>
+                                <input
+                                    className="tournament-text-input"
+                                    type="text"
+                                    value={image.image_name || ''} 
+                                    onChange={(e) => handleFieldChange('image', 'image_name', e.target.value, index)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="edit-button-box">
+                <button type="button" onClick={() => handleEditSubmit()}>제출</button>
             </div>
         </div>
     );
 }
 
 export default TournamentEditPage;
-
-    
